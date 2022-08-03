3b9ccca7f3722cb329e9ef448939528ad690d592
acomarcho <acoxstpd@gmail.com>
2022-08-03 11:16:06 +0700

    added tanggal pengujian and jam pengujian at sheets

--- pages/api/sheets.js
+++ pages/api/sheets.js
@@ -1,11 +1,12 @@
 import path from "path";
+import moment from "moment";
 
 const { google } = require("googleapis");
 const spreadsheetId = "1wz7AZIbQ4zEDo7W4JH252_TXP42ScXptlopLlIwKvOI";
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const { 
+  if (req.method === "POST") {
+    const {
       nama,
       durasi,
       tingkatKantuk,
@@ -19,22 +20,24 @@ export default async function handler(req, res) {
       medianRT,
       mean1OverMeanRT,
       fastest10RT,
-      slowest10RT 
+      slowest10RT,
     } = req.body;
 
     const auth = new google.auth.GoogleAuth({
       credentials: {
         client_email: process.env.GOOGLE_CLIENT_EMAIL,
-        private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n')
+        private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
       },
-      scopes: "https://www.googleapis.com/auth/spreadsheets"
+      scopes: "https://www.googleapis.com/auth/spreadsheets",
     });
-  
+
     const client = await auth.getClient();
-  
-    const googleSheets = google.sheets({version: "v4", auth: client});
-  
+
+    const googleSheets = google.sheets({ version: "v4", auth: client });
+
     try {
+      moment.locale('id');
+      let date = moment().utcOffset(7); /* GMT + 7 */
       await googleSheets.spreadsheets.values.append({
         auth,
         spreadsheetId,
@@ -44,6 +47,8 @@ export default async function handler(req, res) {
           values: [
             [
               nama,
+              date.format('Do MMMM YYYY'),
+              date.format('hh:mm:ss[+07:00]'),
               durasi,
               tingkatKantuk,
               tingkatLelah,
@@ -56,17 +61,17 @@ export default async function handler(req, res) {
               medianRT,
               mean1OverMeanRT,
               fastest10RT,
-              slowest10RT 
-            ]
-          ]
-        }
-      })
+              slowest10RT,
+            ],
+          ],
+        },
+      });
       res.status(200).json({
-        message: "Successfully sent!"
+        message: "Successfully sent!",
       });
     } catch (error) {
       res.status(400).json({
-        message: error
+        message: error,
       });
       console.log(error);
     }
